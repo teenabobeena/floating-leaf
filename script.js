2227630e2aee741ca32cf4f6fc861102f641d345
teenabobeena <christinas.codingemail@gmail.com>
2025-07-09 19:19:52 -0400

    Add script to calculate hero height

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const nav = document.querySelector('nav');
 toggle.addEventListener('click', () => {
     nav.classList.toggle('menu-open');
 });
+
+function setHeroHeight() {
+    const navBar = document.getElementById('navbar');
+    const hero = document.getElementById('hero');
+    const navHeight = navBar.offsetHeight;
+    hero.style.height = `calc(100vh - ${navHeight}px)`;
+}
+
+window.addEventListener('load', setHeroHeight);
+window.addEventListener('resize', setHeroHeight);
